f74737e0f45ac01fddf40e23b4963cc20a459c85
Ducdv <ducdv@MacOne-YP03LLs-MacBook-Pro.local>
2022-03-19 23:06:26 +0700

    create components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <HelloDuc /> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import HelloDuc from './views/components/HelloDuc.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloDuc
   }
 }
 </script>
